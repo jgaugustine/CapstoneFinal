e6581771e6b41d63de4327d713f411f5f2d54e79
jgaugustine <augustine@uni.minerva.edu>
2026-03-03 15:42:58 +0530

    scroll

--- CapstoneHub/src/pages/Index.tsx
+++ CapstoneHub/src/pages/Index.tsx
@@ -99,7 +99,7 @@ export default function Index() {
   }, []);
 
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="h-screen flex flex-col">
       <Header />
       <div className="flex-1 min-h-0 flex flex-col lg:flex-row">
         {/* Mobile: horizontal steps */}
